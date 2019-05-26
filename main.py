25b7f4c5fe361f896b2cef212d4f8ce317831f67
wh2per <93negaro@gmail.com>
2019-05-26 23:52:04 +0900

    Merge Project #2 척추 검사, 방향 검사

--- main.py
+++ main.py
@@ -5,23 +5,47 @@ import numpy as np
 import argparse
 import datetime, time
 import os
+import math
+
+"""
+summary : 두 벡터 사이각을 측정
+parameter[1] - inputA :  시작 Point
+parameter[2] - inputB :  중간 Point
+parameter[3] - inputC :  끝 Point
+"""
+def checkDegree(inputA, inputB, inputC):            # 목, 척추, 골반, 방향
+    # Vector 변환
+    vA = (float(inputA[0] - inputB[0]), float(inputA[1] - inputB[1]))
+    vB = (float(inputC[0] - inputB[0]), float(inputC[1] - inputB[1]))
+
+    # Vector 크기
+    vASize = math.sqrt(vA[0] ** 2 + vA[1] ** 2)
+    vBSize = math.sqrt(vB[0] ** 2 + vB[1] ** 2)
+
+    # Vector 내적
+    vAIn = vA[0] * vB[0] + vA[1] * vB[1]
+
+    # Vector 외적
+    vAOut = vASize * vBSize
+
+    # Radian
+    Rad = math.acos(vAIn / vAOut)
+    print("R : ", Rad)
+    Deg = math.degrees(Rad)
+    print("D : ", Deg)
+
+    if Deg < 145:
+        cv.line(frame, inputA, inputB, (0, 0, 255), 2)
+        cv.line(frame, inputB, inputC, (0, 0, 255), 2)
+        return Deg
+
 
 def allBody():
     for k, v in BODY_PARTS.items():
-        if v < 14:
+        if 1 < v < 14 :
             if points[BODY_PARTS[k]] == None:
                 return False
     return True
-
-
-# 방향 및 척추
-dir = False     # False : 오른쪽, True : 왼쪽
-check = []      # 1,-1을 넣고 sum으로 결과 출력
-pos_neck = -1        # 이전 값
-pos_mid = -1        # 이전 값
-
-# 팔자걸음
-check2 = 0
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input', help='Path to image or video. Skip to capture frames from camera')
@@ -31,67 +55,68 @@ parser.add_argument('--height', default=200, type=int, help='Resize input to spe
 
 args = parser.parse_args()
 
-DirToday = datetime.date.today().strftime("%y%m%d")
-DirTime = time.strftime("%H%M%S")
-print(DirToday)
-print(DirTime)
-mp4Dir = "Video/"+DirToday
+mp4Dir = "Video"
 if not os.path.isdir(mp4Dir):
     os.mkdir(mp4Dir)
-timeDir = os.path.join(mp4Dir, DirTime)
-os.mkdir(timeDir)
+mp4Dir += "/a"
+if not os.path.isdir(mp4Dir):
+    os.mkdir(mp4Dir)
 
-BODY_PARTS = {"Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
-            "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
-              "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
-              "LEye": 15, "REar": 16, "LEar": 17, "Background": 18}
+# DirToday = datetime.date.today().strftime("%y%m%d")
+# DirTime = time.strftime("%H%M%S")
+# print(DirToday)
+# print(DirTime)
+# mp4Dir = "Video/"+DirToday
+# if not os.path.isdir(mp4Dir):
+#     os.mkdir(mp4Dir)
+# timeDir = os.path.join(mp4Dir, DirTime)
+# os.mkdir(timeDir)
 
-# POSE_PAIRS = [["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
-#               ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
-#               ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
-#               ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
-#               ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"]]
-
-# POSE_PAIRS = [["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
-#               ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
-#               ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
-#               ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
-#               ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"]]
+BODY_PARTS = {  "Nose": 0,      "Neck": 1,      "RShoulder": 2,     "RElbow": 3,        "RWrist": 4,
+                "LShoulder": 5, "LElbow": 6,    "LWrist": 7,        "RHip": 8,          "RKnee": 9,
+                "RAnkle": 10,   "LHip": 11,     "LKnee": 12,        "LAnkle": 13,       "REye": 14,
+                "LEye": 15,     "REar": 16,     "LEar": 17,         "Background": 18,   "Waist": 19,
+                "Groin": 20}
 
 POSE_PAIRS = [["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
               ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
              ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["RHip", "LHip"],
               ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"]]
 
-ROI_PAIRS = [480,420,360,300,260,200]
+#ROI_PAIRS = [480,420,360,300,260,200]
 
 inWidth = args.width
 inHeight = args.height
 
 vid_writer = cv.VideoWriter('Video/a_result.mp4',cv.VideoWriter_fourcc('m','p','4','v'), 24, (480,480))
 
-net = cv.dnn.readNetFromTensorflow("graph_opt.pb")
+net = cv.dnn.readNetFromTensorflow("graph_opt.pb")  # ".protobuf" 파일
 
-# cap = cv.VideoCapture(args.input if args.input else 0)
-cap = cv.VideoCapture("sample/a.mp4")
+cap = cv.VideoCapture(args.input if args.input else 0)     # Args or Cam
+#cap = cv.VideoCapture(0)                                   # Cam
+#cap = cv.VideoCapture("a.mp4")                              # a.mp4
 
 index = 100
-tin = 0
 roi = 0
+roiX = 480
+
 while cv.waitKey(1) < 0:
     flag = False
     hasFrame, frame = cap.read()
+
     frame = cv.flip(frame, 1)       # 좌우반전
+    #dst = cv.transpose(frame)
+    #frame = cv.flip(dst, 0)
 
     if not hasFrame:
-        cv.waitKey()
         break
+
     frameWidth = frame.shape[1]
     frameHeight = frame.shape[0]
 
     net.setInput(cv.dnn.blobFromImage(frame, 1.0, (inWidth, inHeight), (127.5, 127.5, 127.5), swapRB=True, crop=False))
     out = net.forward()
-    out = out[:, :19, :, :]  # MobileNet output [1, 57, -1, -1], we only need the first 19 elements
+    out = out[:, :21, :, :]  # MobileNet output [1, 57, -1, -1], we only need the first 19 elements
 
     assert (len(BODY_PARTS) == out.shape[1])
 
@@ -121,74 +146,56 @@ while cv.waitKey(1) < 0:
         idTo = BODY_PARTS[partTo]
 
         if points[idFrom] and points[idTo]:
+            cv.line(frame, points[idFrom], points[idTo], (0, 255, 0), 2)
             cv.line(nframe, points[idFrom], points[idTo], (0, 255, 0), 2)
-            cv.line(nframe, points[idFrom], points[idTo], (0, 255, 0), 2)
-            cv.ellipse(frame, points[idFrom], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
-            cv.ellipse(frame, points[idTo], (3, 3), 0, 0, 360, (0, 0, 255), 3, cv.FILLED)
+            cv.ellipse(frame, points[idFrom], (2, 2), 0, 0, 360, (0, 0, 255), cv.FILLED)
+            cv.ellipse(frame, points[idTo], (2, 2), 0, 0, 360, (0, 0, 255), cv.FILLED)
 
     # 골반 생성
-    a = BODY_PARTS["RHip"]
-    b = BODY_PARTS["LHip"]
-    g = BODY_PARTS["Neck"]
-    f = ()
-    c = ()
-    if points[a] != None and points[b] != None and points[g] != None:
-        # 골반중심
-        c = (int((points[a][0] + points[b][0])/2), int((points[a][1] + points[b][1])/2))
-        d = points[g]
+    indexRHip = BODY_PARTS["RHip"]
+    indexLHip = BODY_PARTS["LHip"]
+    indexNeck = BODY_PARTS["Neck"]
+    indexWaist = BODY_PARTS["Waist"]
+    indexGroin = BODY_PARTS["Groin"]
+    if points[indexRHip] != None and points[indexLHip] != None and points[indexNeck] != None:
+        points[indexGroin] = (int((points[indexRHip][0] + points[indexLHip][0])/2), int((points[indexRHip][1] + points[indexLHip][1])/2))    # c(x,y) : 양쪽 골반 사이 포인트
+
+        cv.ellipse(frame, points[indexGroin], (2, 2), 0, 0, 360, (0, 0, 255), cv.FILLED)
+        cv.ellipse(frame, points[indexNeck], (2, 2), 0, 0, 360, (0, 0, 255), cv.FILLED)
 
         # 척추 생성
-        f = (c[0], int((c[1] + d[1])/2))     # 척추 포인트
-        cv.line(nframe, d, f, (0, 255, 0), 2)       # 목에서 척추
-        cv.line(nframe, f, c, (0, 255, 0), 2)       # 척추에서 골반
+        points[indexWaist] = (points[indexGroin][0], int((points[indexGroin][1] + points[indexNeck][1])/2.2))            # 척추 포인트 : X값은 골반사이포인트값, Y값은 목과 골반사이포인트의 중간점
+        cv.line(frame, points[indexNeck], points[indexWaist], (0, 255, 0), 2)        # 목에서 척추
+        cv.line(frame, points[indexWaist], points[indexGroin], (0, 255, 0), 2)        # 척추에서 골반
+        cv.line(nframe, points[indexNeck], points[indexWaist], (0, 255, 0), 2)  # 목에서 척추
+        cv.line(nframe, points[indexWaist], points[indexGroin], (0, 255, 0), 2)  # 척추에서 골반
 
-    # 이미지 프로세싱 : Grayscale, Thresholding, Resize
-    # nframe = cv.cvtColor(nframe, cv.COLOR_RGB2GRAY)
-    # n_,nframe = cv.threshold(nframe,128,255,cv.THRESH_BINARY)
-    # nframe = cv.resize(nframe, dsize=(120,120))
+    nframe = cv.cvtColor(nframe, cv.COLOR_RGB2GRAY)
+    n_,nframe = cv.threshold(nframe,128,255,cv.THRESH_BINARY)
+    nframe = cv.resize(nframe, dsize=(120,120))
 
+    dir = 1
     # 영상 저장 여부 검사
     if allBody():
-        # print(points[g])  # 목
-        # print(f)          # 척추
-        # print(c)          # 골반
-        if points[g][0] - f[0] > 8:
-            check.append(1)
-        else:
-            check.append(-1)
-
-        l = BODY_PARTS["LHip"]
-        r = BODY_PARTS["RHip"]
-        e = BODY_PARTS["LAnkle"]
-        y = BODY_PARTS["RAnkle"]
-        La = int((points[l][0] + points[e][0])/2)
-        Ra = int((points[r][0] + points[y][0])/2)
-        LK = int(points[BODY_PARTS["LKnee"]][0])
-        RK = int(points[BODY_PARTS["RKnee"]][0])
-        if RK - Ra > 5:
-            check2 += 1
-        if La - LK > 5:
-            check2 += 1
-
-        if roi <= 5 and ROI_PAIRS[roi] >= int((points[a][0] + points[b][0]) / 2):
+        #checkDegree(points[BODY_PARTS["RHip"]],points[BODY_PARTS["RKnee"]],points[BODY_PARTS["RAnkle"]])    # 오른다리 검사
+        #checkDegree(points[BODY_PARTS["LHip"]], points[BODY_PARTS["LKnee"]], points[BODY_PARTS["LAnkle"]])  # 왼다리 검사
+        checkDegree(points[BODY_PARTS["Neck"]], points[BODY_PARTS["Waist"]], points[BODY_PARTS["Groin"]])  # 허리 검사
+        #print(ROI_PAIRS[roi], int((points[a][0] + points[b][0])/2))
+        if roi <= 5 and roiX >= int((points[indexNeck][0] + points[indexGroin][0])/2):
             index += 1
             name = str(index) + ".png"
-            cv.imwrite(os.path.join(timeDir, name), nframe, [cv.IMWRITE_PNG_BILEVEL, 1])
+            #cv.imwrite(os.path.join(timeDir, name), nframe, [cv.IMWRITE_PNG_BILEVEL, 1])
+            cv.imwrite("Video/a/" + name, nframe, [cv.IMWRITE_PNG_BILEVEL, 1])
             roi += 1
-        # tin += 1
-        # if tin % 3 == 0:
-        # index += 1
-        # name = str(index) + ".png"
-        # cv.imwrite(os.path.join(timeDir, name), nframe, [cv.IMWRITE_PNG_BILEVEL, 1])
+            roiX -= 60
 
-    t, _ = net.getPerfProfile()
-    freq = cv.getTickFrequency() / 1000
-    cv.putText(frame, '%.2fms' % (t / freq), (10, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-    if sum(check) > 3:
-        cv.putText(frame, "Dangerous!!", (10,40),cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
-    if check2 > 5 :
-        cv.putText(frame, "Dangerous~~~", (10, 60), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0))
+    #t, _ = net.getPerfProfile()
+    #freq = cv.getTickFrequency() / 1000
+    #cv.putText(frame, '%.2fms' % (t / freq), (10, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+
     cv.imshow('OpenPose using OpenCV', frame)
     cv.imshow('new',nframe)
+
     vid_writer.write(frame)
+
 vid_writer.release()
